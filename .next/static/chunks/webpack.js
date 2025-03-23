f894444c1b25156322b799dd916178ebf49a4884
Prakash Nidhi Verma <pnidhiverma1999@gmail.com>
2025-03-23 16:31:58 -0700

    black color changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "808413d45d9be0cb"; }
+/******/ 		__webpack_require__.h = function() { return "2c607dfc3c704010"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
